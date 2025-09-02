a58a0c420b234d789eadd7a934d55c7b0b00f66d
jharopr <pcvr2706@gmail.com>
2025-09-02 16:40:24 -0500

    Reserva de citas y muestra de citas hechos

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,13 +1,24 @@
-from fastapi import APIRouter, HTTPException
-from app.schemas.user import UserLogin
+from fastapi import APIRouter, HTTPException, status
+from app.schemas.user import UserLogin, AuthenticatedUser
 from app.services.user_service import login_user
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
-@router.post("/login")
+
+@router.post("/login", response_model = AuthenticatedUser)
 def login(data: UserLogin):
-    user = login_user(data.username, data.password)
-    if user:
-        return {"message": "✅ Login exitoso",
-            "user": data.username}
-    raise HTTPException(status_code=401, detail="❌ Credenciales inválidas")
+    user_data = login_user(data.username, data.password)
+    
+    if user_data:
+        return {
+            "id" : user_data["id"],
+            "nombres": user_data["nombres"] , # Return the full user data here
+            "apellidos": user_data["apellidos"],
+            "correo": user_data["correo"],
+            "cod_estudiante": user_data["cod_estudiante"]
+        }
+    
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="❌ Credenciales inválidas"
+    )
