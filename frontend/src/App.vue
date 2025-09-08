4c7eabad1ef29fe560639edfe2956c9fcc161592
Alexis <alexis.garay.y@uni.pe>
2025-09-08 17:02:27 -0500

    Deje el Navbar limpio y quite componentes/vistas no utilizados

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,10 +11,6 @@ const route = useRoute()
       <RouterLink class="navbar-brand text-white fw-semibold" to="/">MedUNI</RouterLink>
       <div class="collapse navbar-collapse show">
         <ul class="navbar-nav ms-auto gap-2">
-          <li class="nav-item" v-if="auth.isAuth"><RouterLink class="nav-link text-white" to="/reservar">Reservar</RouterLink></li>
-          <!-- Mostrar Especialidades -->
-          <li class="nav-item" v-if="auth.isAuth"><RouterLink class="nav-link text-white" to="/especialidades">Especialidades</RouterLink></li>
-          <!-- Mostrar Especialidades  -->
           <li class="nav-item" v-if="!auth.isAuth"><RouterLink class="btn btn-light btn-sm" to="/login">Ingresar</RouterLink></li>
           <li class="nav-item" v-else><button class="btn btn-outline-light btn-sm" @click="auth.logout()">Salir</button></li>
         </ul>
