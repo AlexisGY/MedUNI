153fa524ad6e9d1f2531409d12ebeba2f4d1b225
Alexis <alexis.garay.y@uni.pe>
2025-09-13 21:44:10 -0500

    Correcion de los nombres a CamelCase

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import Auth
 from app.db import getConnection
-from app.routers import CitasRouter, DiasDisponiblesRouter, EspecialidadesRouter, HorariosRouter, MedicosRouter
+from app.routers import Auth, CitasRouter, DiasDisponiblesRouter, EspecialidadesRouter, HorariosRouter, MedicosRouter
 
 # routers 
 
@@ -27,9 +26,10 @@ def root():
     if conn:
         with conn.cursor() as cur:
             cur.execute("SELECT version();")
-            version = cur.fetchone()
-        conn.close()
-        return {"status": "ok", "db_version": version[0]}
+            row = cur.fetchone()
+            db_version = row[0] if row else None
+
+        return {"status": "ok", "db_version": db_version}
     else:
         return {"status": "error", "message": "No se pudo conectar a la base de datos"}
 
